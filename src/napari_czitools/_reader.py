ac8cc02f639928bd6445cca2d5caf01bbfc78cd8
Rhode, Sebastian <sebastian.rhode@zeiss.com>
2025-04-08 17:27:56 +0200

    updates

--- src/napari_czitools/_reader.py
+++ src/napari_czitools/_reader.py
@@ -8,19 +8,7 @@ logger = logging_tools.set_logging()
 
 
 def napari_get_reader(path: str):
-    """A basic implementation of a Reader contribution.
 
-    Parameters
-    ----------
-    path : str or list of str
-        Path to file, or list of paths.
-
-    Returns
-    -------
-    function or None
-        If the path is a recognized format, return a function that accepts the
-        same path or list of paths, and returns a list of layer data tuples.
-    """
     if isinstance(path, list):
         # reader plugins may be handed single path, or a list of paths.
         # if it is a list, it is assumed to be an image stack...
@@ -38,27 +26,6 @@ def napari_get_reader(path: str):
 def reader_function(
     path: str, zoom=1.0, use_dask=False, chunk_zyx=False, use_xarray=True
 ):
-    """Take a path or list of paths and return a list of LayerData tuples.
-
-    Readers are expected to return data as a list of tuples, where each tuple
-    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
-    both optional.
-
-    Parameters
-    ----------
-    path : str or list of str
-        Path to file, or list of paths.
-
-    Returns
-    -------
-    layer_data : list of tuples
-        A list of LayerData tuples where each tuple in the list contains
-        (data, metadata, layer_type), where data is a numpy array, metadata is
-        a dict of keyword arguments for the corresponding viewer.add_* method
-        in napari, and layer_type is a lower-case string naming the type of
-        layer. Both "meta", and "layer_type" are optional. napari will
-        default to layer_type=="image" if not provided
-    """
 
     # call the function to add the data to the viewer
     _add_czi_data(
--- src/napari_czitools/_sample_data.py
+++ src/napari_czitools/_sample_data.py
@@ -4,7 +4,7 @@ from napari_czitools._reader import reader_function
 def celldivision_data():
     """Opens 5D CZI image dataset"""
 
-    filepath = "src/napari_czitools/sample_data/CellDivision_T15_Z20_CH2_X600_Y500_DCV_ZSTD.czi"
+    filepath = r"src/napari_czitools/sample_data/CellDivision_T15_Z20_CH2_X600_Y500_DCV_ZSTD.czi"
 
     return reader_function(filepath)
 
@@ -12,6 +12,14 @@ def celldivision_data():
 def wellplate_data():
     """Opens 6D CZI image dataset"""
 
-    filepath = "src/napari_czitools/sample_data/testwell96_A1-D12_S48_T1_C2_Z1_X640_Y480_ZSTD.czi"
+    filepath = r"src/napari_czitools/sample_data/testwell96_A1-D12_S48_T1_C2_Z1_X640_Y480_ZSTD.czi"
+
+    return reader_function(filepath)
+
+
+def zstack_data():
+    """Opens 3D CZI image dataset"""
+
+    filepath = r"src/napari_czitools/sample_data/RatBrain_Z79_ZSTD.czi"
 
     return reader_function(filepath)
