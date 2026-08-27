f93600cda72da5f9ee31111e637f428983582e12
Rhode, Sebastian <sebastian.rhode@zeiss.com>
2026-08-27 14:24:54 +0200

    fix tests

--- src/napari_czitools/_tests/test_file_changed.py
+++ src/napari_czitools/_tests/test_file_changed.py
@@ -1,22 +1,35 @@
 # File: _test/test_file_changed.py
 
-from napari_czitools._widget import CziReaderWidget
+from napari_czitools._widget import CziReaderWidget, SliderType
 
 
-def file_changed_updates_sliders_correctly(qtbot, mocker):
-    widget = CziReaderWidget(mocker.Mock())
+def _patch_metadata_loading(mocker, widget, metadata):
+    metadata.scene_shape_is_consistent = True
+    mocker.patch(
+        "napari_czitools._widget.CziMetadata",
+        return_value=metadata,
+    )
+    mocker.patch.object(widget, "_update_scene_diff_ui")
+    mocker.patch(
+        "napari_czitools._widget.czimd.create_md_dict_nested",
+        return_value={},
+    )
+    mocker.patch(
+        "napari_czitools._widget.czimd.create_md_dict_red",
+        return_value={},
+    )
+
+
+def test_file_changed_updates_sliders_correctly(qtbot, mocker):
+    widget = CziReaderWidget(mocker.Mock(), sliders=SliderType.TwoSliders)
     mock_metadata = mocker.Mock()
     mock_metadata.image.SizeS = 5
     mock_metadata.image.SizeT = 10
     mock_metadata.image.SizeC = 3
     mock_metadata.image.SizeZ = 15
-    mocker.patch(
-        "czitools.metadata_tools.czi_metadata.CziMetadata",
-        return_value=mock_metadata,
-    )
+    _patch_metadata_loading(mocker, widget, mock_metadata)
 
     widget.filename_edit.value = "test_file.czi"
-    widget._file_changed()
 
     assert widget.scene_slider.enabled
     assert widget.scene_slider.min_slider.min == 0
@@ -35,20 +48,16 @@ def file_changed_updates_sliders_correctly(qtbot, mocker):
     assert widget.z_slider.max_slider.max == 14
 
 
-def file_changed_resets_sliders_when_metadata_is_none(qtbot, mocker):
-    widget = CziReaderWidget(mocker.Mock())
+def test_file_changed_resets_sliders_when_metadata_is_none(qtbot, mocker):
+    widget = CziReaderWidget(mocker.Mock(), sliders=SliderType.TwoSliders)
     mock_metadata = mocker.Mock()
     mock_metadata.image.SizeS = None
     mock_metadata.image.SizeT = None
     mock_metadata.image.SizeC = None
     mock_metadata.image.SizeZ = None
-    mocker.patch(
-        "czitools.metadata_tools.czi_metadata.CziMetadata",
-        return_value=mock_metadata,
-    )
+    _patch_metadata_loading(mocker, widget, mock_metadata)
 
     widget.filename_edit.value = "test_file.czi"
-    widget._file_changed()
 
     assert not widget.scene_slider.enabled
     assert widget.scene_slider.min_slider.min == 0
@@ -67,16 +76,15 @@ def file_changed_resets_sliders_when_metadata_is_none(qtbot, mocker):
     assert widget.z_slider.max_slider.max == 0
 
 
-def file_changed_enables_load_pixeldata_button(qtbot, mocker):
-    widget = CziReaderWidget(mocker.Mock())
+def test_file_changed_enables_load_pixeldata_button(qtbot, mocker):
+    widget = CziReaderWidget(mocker.Mock(), sliders=SliderType.TwoSliders)
     mock_metadata = mocker.Mock()
     mock_metadata.image.SizeS = 5
-    mocker.patch(
-        "czitools.metadata_tools.czi_metadata.CziMetadata",
-        return_value=mock_metadata,
-    )
+    mock_metadata.image.SizeT = None
+    mock_metadata.image.SizeC = None
+    mock_metadata.image.SizeZ = None
+    _patch_metadata_loading(mocker, widget, mock_metadata)
 
     widget.filename_edit.value = "test_file.czi"
-    widget._file_changed()
 
     assert widget.load_pixeldata.enabled
